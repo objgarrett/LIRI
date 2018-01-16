696038428d6f9f7e17bcab9d02f9fc2debe84529
Olivia Garrett <oliviajonesgarrett@gmail.com>
2018-01-15 21:14:50 -0500

    setting up project

---
+++ javascript/keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.56LOCPUNhDTWlifluey5nmfDp,
+  consumer_secret: process.env.1qO1gbgRC6OaSXz0S9zCXSudnhUc8tPuaRRvBOsYeSvGi3EHJT,
+  access_token_key: process.env.953077605766455296-lJVSVa5HHZGBOb1mDRxWSrNnfBQ8FNh,
+  access_token_secret: process.env.VOHn3v9416Jo9nXwwTrtMb4wdUPOKfwTbJPwVRU3f9YRK
+
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
